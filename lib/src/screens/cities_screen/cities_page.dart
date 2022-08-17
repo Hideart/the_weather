8318c9b40c5f8b921f28de61976ba2897f595a4e
Hideart <daniil@hideart.ru>
2022-08-17 17:10:17 +0300

    Refactored service locator

--- lib/src/screens/cities_screen/cities_page.dart
+++ lib/src/screens/cities_screen/cities_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_weather/src/screens/cities_screen/bloc/cities_cubit.dart';
 import 'package:the_weather/src/screens/cities_screen/cities_view.dart';
-import 'package:the_weather/src/utils/service_locator.dart';
 
 class CitiesPage extends StatelessWidget {
   const CitiesPage({Key? key}) : super(key: key);
@@ -10,8 +9,7 @@ class CitiesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          DI.get<CitiesCubit>(instanceName: 'cities')..getCities(page: 1),
+      create: (context) => CitiesCubit()..getCities(page: 1),
       child: const CitiesView(),
     );
   }
--- lib/src/utils/service_locator.dart
+++ lib/src/utils/service_locator.dart
@@ -3,9 +3,6 @@ import 'package:get_it/get_it.dart';
 import 'package:the_weather/packages/repositories/geo_data/geo_data_repository.dart';
 import 'package:the_weather/packages/repositories/weather/weather_repository.dart';
 import 'package:the_weather/src/app_state/geo/geo_cubit.dart';
-import 'package:the_weather/src/screens/cities_screen/bloc/cities_cubit.dart';
-import 'package:the_weather/src/screens/countries_screen/bloc/countries_cubit.dart';
-import 'package:the_weather/src/screens/home_screen/bloc/weather_cubit.dart';
 
 class DI {
   static final GetIt _locator = GetIt.instance;
@@ -15,21 +12,9 @@ class DI {
       () => GeoDataRepository(dotenv.env['RAPID_API_KEY']),
       instanceName: 'geodata',
     );
-    DI.registerFactory<WeatherRepository>(
+    DI.registerLazySingleton<WeatherRepository>(
       () => WeatherRepository(dotenv.env['OWM_API_KEY']),
       instanceName: 'weather',
-    );
-    DI.registerFactory<WeatherCubit>(
-      () => WeatherCubit(),
-      instanceName: 'weather_cubit',
-    );
-    DI.registerFactory<CountriesCubit>(
-      () => CountriesCubit(),
-      instanceName: 'countries',
-    );
-    DI.registerFactory<CitiesCubit>(
-      () => CitiesCubit(),
-      instanceName: 'cities',
     );
     DI.registerSingleton<GeoStateCubit>(
       GeoStateCubit(),
